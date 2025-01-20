b43f217bf5a2c9fe4cff6545175f5cf96b25edc3
Ben Szabo <10125940+b3nk3@users.noreply.github.com>
2025-01-20 14:35:56 +0000

    feat: add get_secret_for_nodejs_22_lambda.sh

---
+++ get_secret_for_nodejs_22_lambda.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# This function will export all key value pairs under a given AWS Secret as an environment variable file
+
+echo "Exporting variables from Secrets Manager"
+
+# Get all Key Value pairs of the secret
+SECRETS_JSON=$(aws secretsmanager get-secret-value \
+  --region ${REGION} \
+  --secret-id ${SECRET_PATH} \
+  --query 'SecretString' \
+  --output text)
+
+VARS_FILE="/tmp/secrets"
+rm -f $VARS_FILE # Remove the file if it already exists
+# Lopp through each key value pair and export it as an environment variable
+for row in $(echo "${SECRETS_JSON}" | jq -r "to_entries|map(\"\(.key)=\(.value|tostring)\")|.[]"); do
+  export $row
+done
+echo "Variables from Secrets Manager successfully exported"
+# Execute the lambda function using the entrypoint script from https://github.com/aws/aws-lambda-base-images/blob/nodejs22.x/Dockerfile.nodejs22.x
+exec /lambda-entrypoint.sh "$@"
